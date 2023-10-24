7a618ded61e9be62df992f81a746a1d8dd1fb145
Eduardo Chavez Martin <66572435+eduardohufg@users.noreply.github.com>
2023-10-24 10:29:48 -0600

    firts commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let carrito = [];
+
+function agregarAlCarrito(nombre, precio) {
+    carrito.push({nombre, precio});
+    mostrarCarrito();
+}
+
+function mostrarCarrito() {
+    const lista = document.getElementById('carrito');
+    lista.innerHTML = '';
+    let total = 0;
+    
+    for(let item of carrito) {
+        total += item.precio;
+        const li = document.createElement('li');
+        li.textContent = `${item.nombre} - $${item.precio}`;
+        lista.appendChild(li);
+    }
+    
+    const liTotal = document.createElement('li');
+    liTotal.textContent = `Total: $${total}`;
+    lista.appendChild(liTotal);
+}
