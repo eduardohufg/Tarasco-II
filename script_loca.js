71a6c46923a0f326cf5a68fafd1d3995dc4a79d6
Joel Vargas <joel@MacBook-Pro-de-Joel.local>
2023-10-25 12:06:49 -0600

    res con botones

--- script_loca.js
+++ script_loca.js
@@ -26,3 +26,15 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
 }
+
+// Event listener para las teclas de flecha izquierda y derecha
+window.addEventListener('keydown', function(e) {
+  switch(e.key) {
+    case 'ArrowLeft':
+      plusSlides(-1);  // Mueve al slide anterior
+      break;
+    case 'ArrowRight':
+      plusSlides(1);  // Mueve al slide siguiente
+      break;
+  }
+});
